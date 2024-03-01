3b25c7b69b0a17cda3bd2ab64f2a9c82c3ffa949
Alexandru Bej <alexbej07@gmail.com>
2024-03-01 19:55:55 +0100

    touch design of side modal

--- src/components/side-modal/side-modal.component.jsx
+++ src/components/side-modal/side-modal.component.jsx
@@ -1,16 +1,15 @@
 import { NavLink, Link } from 'react-router-dom';
 
+import Button from '../button/button.component';
+
 import './side-modal.styles.scss';
 
 const SideModal = ({ isSideModalOpen, onSideModalClose }) => {
-    if (!isSideModalOpen) {
-        return null;
-    }
     return (
-        <div className='side-modal-overlay' onClick={onSideModalClose}>
+        <div className={'side-modal-overlay' + (isSideModalOpen ? ' width-full' : '')} onClick={onSideModalClose}>
             <div className='side-modal-container'>
                 <main className='side-modal-content' onClick={e => e.preventDefault()}>
-                    <button onClick={onSideModalClose}>Close</button>
+                    <Button onClick={onSideModalClose}>Close</Button>
 
                     <div className='side-nav-links'>
                         <NavLink className="nav-link" activeclassname="active" onClick={onSideModalClose} to='/'>
