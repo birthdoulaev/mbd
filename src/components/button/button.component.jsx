143717115baae3bf2bb26ce3db02efd0d2397299
Alexandru Bej <alexbej07@gmail.com>
2024-03-07 15:26:43 +0200

    add spinner

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,10 +1,14 @@
+import Spinner from '../spinner/spinner.component';
+
 import './button.styles.scss';
 
 const Button = ({ children, isLoading = false, customClassName = '', ...otherProps }) => {
     return (
         <div className={`${customClassName} btn-container`}>
             <button className='mbd-btn' disabled={isLoading} {...otherProps}>
-                <span>{children}</span>
+                {
+                    isLoading ? <Spinner /> : <span>{children}</span>
+                }
             </button>
         </div>
     );
