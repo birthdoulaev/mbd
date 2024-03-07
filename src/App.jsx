8f7d832908fe35c2b2ddeb18a39f10e13972358b
Alexandru Bej <alexbej07@gmail.com>
2024-03-07 18:48:38 +0200

    added not found component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './routes/home/home.component';
 import About from './routes/about/about.component';
 import Services from './routes/services/services.component';
 import Contact from './routes/contact/contact.component';
+import NotFound from './routes/not-found/not-found.component';
 
 import './App.scss';
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='services' element={<Services />} />
         <Route path='contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/not-found/not-found.component.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+import NotFoundImg from '../../assets/404.png';
+
+import './not-found.styles.scss';
+
+const NotFound = () => {
+    return (
+        <div className="not-found-page">
+            <h1>Oops! It seems like you've reached a dead end</h1>
+            <p>The page you're looking for might have been removed, renamed, or is temporarily unavailable</p>
+            <img src={NotFoundImg} alt="404 Illustration" className="not-found-img" />
+        </div>
+    )
+}
+
+export default NotFound;
